a1945faa27a831c3b7913844ef8ea0b824779309
Stephany-Doris <stephany.awino@twiga.com>
2021-12-26 21:24:45 +0300

    add component styles and button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Our Pricing</h1>
+  <p>Only pay for what you need</p>
+  <div class="card-wrapper">
+    <PricingCard title="Free">
+      <span>$24.99</span>
+      <ul>
+        <li>storage</li>
+      </ul>
+    </PricingCard>
+    <PricingCard title="Basic" />
+    <PricingCard title="Pro" />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+import PricingCard from "./components/PricingCard.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "App",
+  setup() {},
   components: {
-    HelloWorld
-  }
-}
+    PricingCard,
+  },
+});
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3em;
+  font-size: 18px !important;
+}
+
+.card-wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 2em;
+  padding: 20px;
+  max-width: inherit;
+}
+span {
+  font-size: 2em;
+}
+ul {
+  list-style: none;
+  padding: 12px;
+  border-top: 1px solid #eee;
+  border-bottom: 1px solid #eee;
+}
+
+.card:hover span {
+  font-size: 3em;
+}
+
+.card:hover ul {
+  border-top: 1px solid #2e2f30;
+  border-bottom: 1px solid #2e2f30;
 }
 </style>
---
+++ src/components/PricingCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <slot></slot>
+    <Button text="Subscribe" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+
+export default defineComponent({
+  name: "PricingCard",
+  props: {
+    title: String,
+  },
+  components: { Button },
+  setup() {},
+});
+</script>
+
+<style scoped>
+.card {
+  height: 450px;
+  width: -webkit-fill-available;
+  background-color: white;
+  color: #050710;
+  border-radius: 8px;
+  margin: auto;
+  padding: 8px;
+  position: relative;
+}
+
+.card:hover {
+  height: 500px;
+  /* flex-grow: 2; */
+  color: white;
+  background-color: #050710;
+}
+</style>
